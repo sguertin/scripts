71bf34f00cdb8b71eae3e2b395cc5430c6fb51e5
Scott Guertin <sguertin@gmail.com>
2022-09-06 10:37:59 -0400

    Remove New-WebProxy dependency

--- Reset-Date.ps1
+++ Reset-Date.ps1
@@ -1,5 +1,6 @@
 function Reset-Date {
-    $time = New-WebServiceProxy http://www.satan.lt/WebServices/Time.asmx; 
-    $currentDate = Set-Date -Date ($time.GetServerLocalTime()); 
-    Write-Output "Date/Time Reset to " + $currentDate.toString("yyyy-MM-dd hh:mm:sstt");
+    $URL = "http://api.timezonedb.com/v2.1/get-time-zone?key=JXVVWAJGK0JM&format=json&by=zone&zone=America/New_York";
+    $timeInfo = (Invoke-WebRequest -Uri $URL -Method Get).Content | ConvertFrom-Json;    
+    $currentDate = Set-Date -Date ([DateTime]::Parse($timeInfo.formatted)); 
+    Write-Output ("Date/Time Reset to " + $currentDate.toString("yyyy-MM-dd hh:mm:sstt"));
 }
